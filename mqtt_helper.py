405683bc4f47323608292b403d49a734c25b63d5
David <david.erving@hotmail.com>
2020-02-22 23:14:15 +1100

    fix

--- mqtt_helper.py
+++ mqtt_helper.py
@@ -14,20 +14,18 @@ class mqtt_helper():
         self.value_topic = "home/inside/sensor/"+str(location)
         self.status_topic = "status/sensor/"+str(location)
         self.client_label = str(location)+"_conditions"
-
         self.client = mqtt.Client(self.client_label)
+
         offline_msg = json.dumps({"location":self.location, "status":"offline"})
         client.will_set(self.status_topic, payload=offline_msg, qos=0, retain=True)
         client.connect(self.host, keepalive=60)
 
     def publish_message(self, temp, hum, batt):
-
         dict_msg = {"location":self.location, "temperature":temp, "humidity":hum, "battery":batt}
         msg = json.dumps(dict_msg)
-    	client.publish(self.value_topic,msg)	
-
+    	client.publish(self.value_topic,msg)
+    
     def publish_status(self):
-
         online_msg = json.dumps({"location":self.location, "status":"online"})
         client.publish(self.status_topic, payload=online_msg, qos=0, retain=True)
 
