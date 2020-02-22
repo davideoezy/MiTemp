c8801c8c468fd6eac1fada6a7f5674d63f5c6298
David <david.erving@hotmail.com>
2020-02-22 23:10:37 +1100

    gitflow-feature-stash: branch-create-mqtt-class

--- read_conditions_lounge.py
+++ read_conditions_lounge.py
@@ -5,35 +5,24 @@
 from bluepy import btle
 import os
 import re
-#from dataclasses import dataclass
 import time
-import paho.mqtt.client as mqtt
-import json
-import mysql.connector as mariadb
+#import mysql.connector as mariadb
+from mqtt_helper import mqtt_helper
 
-adress = 'A4:C1:38:6B:B1:CB'
+mqtt_helper = mqtt_helper()
+
+address = 'A4:C1:38:6B:B1:CB'
 location = 'lounge'
 
-topic = "home/inside/sensor/"+str(location)
-status_topic = "status/heater"
+# device_label='RPi_1'
 
-server_address="192.168.0.10" 
+# db_host = '192.168.0.10'
+# db_host_port = '3306'
+# db_user = 'rpi'
+# db_pass = 'warm_me'
+# db = 'temp_logger'
 
-client_label = str(location)+"_conditions"
-client = mqtt.Client(client_label)
-
-offline_msg = json.dumps({"location":location, "status":"offline"})
-client.will_set(status_topic, payload=offline_msg, qos=0, retain=True)
-
-client.connect(server_address, keepalive=60)
-
-device_label='RPi_1'
-
-db_host = '192.168.0.10'
-db_host_port = '3306'
-db_user = 'rpi'
-db_pass = 'warm_me'
-db = 'temp_logger'
+mqtt_helper.initialise(location)
 
 class MyDelegate(btle.DefaultDelegate):
 	def __init__(self, params):
@@ -53,44 +42,31 @@ class MyDelegate(btle.DefaultDelegate):
 			batt=p.readCharacteristic(0x001b)
 			batt=int.from_bytes(batt,byteorder="little")
 			
-			publish_message(location, temp, humidity, batt)
-			publish_status(location)
+			mqtt_helper.publish_message(temp, humidity, batt)
+			mqtt_helper.publish_status()
 
-			insert_stmt = """
-    		INSERT INTO temperature
-    		(device, temp)
-    		VALUES
-    		('{}',{})""".format(device_label,temp)
+			# insert_stmt = """
+    		# INSERT INTO temperature
+    		# (device, temp)
+    		# VALUES
+    		# ('{}',{})""".format(device_label,temp)
 						
-			con = mariadb.connect(host = db_host, port = db_host_port, user = db_user, password = db_pass, database = db)
-			cur = con.cursor()
+			# con = mariadb.connect(host = db_host, port = db_host_port, user = db_user, password = db_pass, database = db)
+			# cur = con.cursor()
 			
-			try:
-				cur.execute(insert_stmt)
-				con.commit()
-			except:
-				con.rollback()
-				con.close()
+			# try:
+			# 	cur.execute(insert_stmt)
+			# 	con.commit()
+			# except:
+			# 	con.rollback()
+			# 	con.close()
 
 		except Exception as e:
 			print("Fehler")
 			print(e)
 	
-def publish_message(location, temp, hum, batt):
-
-	dict_msg = {"location":location, "temperature":temp, "humidity":hum, "battery":batt}
-	#str_msg = str(measurement) + ", value=" + str(reading)
-	msg = json.dumps(dict_msg)
-
-	client.publish(topic,msg)	
-
-def publish_status(location):
-	status_topic = "status/heater"
-	online_msg = json.dumps({"location":location, "status":"online"})
-	client.publish(status_topic,payload=online_msg, qos=0, retain=True)
-
 def connect():
-	p = btle.Peripheral(adress)	
+	p = btle.Peripheral(address)	
 	val=b'\x01\x00'
 	p.writeCharacteristic(0x0038,val,True)
 	p.withDelegate(MyDelegate("abc"))
